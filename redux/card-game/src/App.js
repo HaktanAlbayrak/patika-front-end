141cf137d39ca77c154934865189dbceb11b398b
enes vardar <enesvrdr0@gmail.com>
2022-02-12 20:34:37 +0300

    card-game

---
+++ redux/card-game/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { CardList } from './components/CardList';
+
+function App() {
+  return (
+    <div className="App">
+
+      <CardList/>
+
+    </div>
+  );
+}
+
+export default App;
